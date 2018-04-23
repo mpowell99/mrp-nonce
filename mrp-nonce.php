1c598cd730b0fa4c21ab2bcf6c86d5b5828e18d4
Michael Powell <mpowell99@gmail.com>
2018-04-23 13:13:39 +0200

    Updated Version Number

--- mrp-nonce.php
+++ mrp-nonce.php
@@ -3,14 +3,14 @@
  * MRP Nonce
  *
  * @link              https://www.criticker.com
- * @since             1.0.0
+ * @since             1.1.0
  * @package           MRP_Nonce
  *
  * @wordpress-plugin
  * Plugin Name:       MRP Nonce
  * Plugin URI:        http://example.com/plugin-name-uri/
  * Description:       An object-oriented implementation of WP-Nonces
- * Version:           1.0.0
+ * Version:           1.1.0
  * Author:            Michael Ross Powell
  * Author URI:        https://www.criticker.com/
  * License:           GPL-2.0+
@@ -20,7 +20,7 @@
 if (!class_exists('MRP_Nonce')) {
     class MRP_Nonce {
         private $mrp_nonce;
-        private $ays_string = 'Are You Sure?';
+        private $nonce_name = '_mrpnonce';
 
         /* The Nonce string is created when the object is instantiated */
         public function __construct() {
@@ -32,18 +32,36 @@ if (!class_exists('MRP_Nonce')) {
             return($this->mrp_nonce);
         }
 
+        /* Display the Are You Sure string, to confirm action */
         public function show_ays($action) {
             wp_nonce_ays($action);
+            return(TRUE);
         }
 
-        /* Retrieve the Are You Sure string */
-        public function get_ays() {
-            return($this->ays_string);
+        /* Return the name suffixed to nonce fields in forms */
+        public function get_nonce_name() {
+            return($this->nonce_name);
         }
 
-        /* Set the Are You Sure string */
-        public function set_ays($string) {
-            $this->ays_string = $string;
+        /* Set a new name for nonce fields */
+        public function set_nonce_name($string) {
+            $this->nonce_name = $string;
+            return(TRUE);
+        }
+
+        /* Return a string with a hidden input field for use in forms */
+        public function get_nonce_field() {
+            return(wp_nonce_field(-1, $this->nonce_name));
+        }
+
+        /* Return a URL with a nonce for use in forms */
+        public function get_nonce_url($url) {
+            return(wp_nonce_url($url, -1, $this->nonce_name));
+        }
+
+        /* Verify the nonce is still valid */
+        public function verify() {
+            return(wp_verify_nonce($this->mrp_nonce));
         }
     }
 }
--- tests/test1.php
+++ tests/test1.php
@@ -16,18 +16,36 @@
 $mynonce = new MRP_Nonce();
 
 echo "Getting Nonce: ";
-echo $mynonce->get_nonce()."\n";
+echo $mynonce->get_nonce()."\n\n";
 
-echo "Retrieving AYS String: ";
-$ays_string = $mynonce->get_ays();
-echo "$ays_string\n";
+/*
+echo "Displaying AYS Message\n";
+$mynonce->show_ays('log-out');
+*/
 
-$new_ays = 'Please think about this carefully, are you really sure?';
-echo "Setting AYS String to ".$new_ays."\n";
-$mynonce->set_ays($new_ays);
+echo "Getting Nonce Name: ";
+echo $mynonce->get_nonce_name()."\n\n";
 
-echo "Retrieving AYS String: ";
-$ays_string = $mynonce->get_ays();
-echo "$ays_string\n";
+echo "Getting Hidden Field for Form\n";
+echo $mynonce->get_nonce_field()."\n\n";
+
+$new_noncename = '_coolnonce';
+echo "Setting New Nonce Name: $new_noncename\n\n";
+$mynonce->set_nonce_name($new_noncename);
+
+echo "Getting a Hidden Field with New Name\n";
+echo $mynonce->get_nonce_field()."\n\n";
+
+echo "Getting URL with Nonce\n";
+$url = "https://www.example.com/login.php";
+echo $mynonce->get_nonce_url($url)."\n\n";
+
+echo "Verifying Nonce\n";
+$is_valid = $mynonce->verify();
+if ($is_valid) {
+    echo $mynonce->get_nonce()." is valid (return code: $is_valid)\n\n";
+} else {
+    echo $mynonce->get_nonce()." is invalid";
+}
 
 ?>
